232176d751bf29517e536fbc9886ce89285aa3ce
luis pimenta <luis.pimenta.it@gmail.com>
2022-12-30 14:58:38 +0000

    upgrade baileys and add forever

--- whatsapp.js
+++ whatsapp.js
@@ -80,7 +80,7 @@ const createSession = async (sessionId, isLegacy = false, res = null) => {
     if (!isLegacy) {
         store.readFromFile(sessionsDir(`${sessionId}_store.json`))
         setInterval(() => {
-          store.writeToFile(`${sessionId}_store.json`)
+          store.writeToFile(sessionsDir(`${sessionId}_store.json`))
         }, 10_000)
         store.bind(wa.ev)
     }
